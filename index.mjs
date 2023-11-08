54c64cf558639c5f0c95ca943b156d7255fa32af
VALEN-PC\arion <valentingr080@gmail.com>
2023-11-08 12:37:39 +0100

    API v1

--- index.mjs
+++ index.mjs
@@ -1,7 +1,5 @@
-import { ObjectId } from "mongodb";
-import products from "./conn.mjs";
 import express from "express";
-import { getFiltros, getSortByDate } from "./help.mjs";
+import v1 from "./v1.mjs";
 
 const app = express();
 const port = 5001;
@@ -13,73 +11,4 @@ app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
 });
 
-app.get("/", async (req, res) => {
-  try {
-    const filtro = getFiltros(req);
-    const sortOption = getSortByDate(req);
-
-    const offset = parseInt(req.query.offset) || 0;
-    const limit = parseInt(req.query.limit) || 0;
-
-    let results = await products
-      .find(filtro)
-      .sort(sortOption)
-      .skip(offset)
-      .limit(limit)
-      .toArray();
-    res.send(results).status(200);
-  } catch (e) {
-    res.send(e).status(500);
-  }
-});
-
-app.post("/", async (req, res) => {
-  try {
-    const product = req.body;
-    const result = await products.insertOne(product);
-    res.send(result).status(200);
-  } catch (e) {
-    res.send(e).status(500);
-  }
-});
-
-app.get("/:id", async (req, res) => {
-  try {
-    const result = await products.findOne({ _id: new ObjectId(req.params.id) });
-    res.send(result).status(200);
-  } catch (e) {
-    res.send(e).status(500);
-  }
-});
-
-app.delete("/:id", async (req, res) => {
-  try {
-    const result = await products.deleteOne({
-      _id: new ObjectId(req.params.id),
-    });
-    res.send(result).status(200);
-  } catch (e) {
-    res.send(e).status(500);
-  }
-});
-
-app.delete("/", async (req, res) => {
-  try {
-    let result = await products.deleteMany(req.body);
-    res.send(result).status(200);
-  } catch (e) {
-    res.send(e).status(500);
-  }
-});
-
-app.put("/:id", async (req, res) => {
-  try {
-    const result = await products.updateOne(
-      { _id: new ObjectId(req.params.id) },
-      { $set: req.body }
-    );
-    res.send(result).status(200);
-  } catch (e) {
-    res.send(e).status(500);
-  }
-});
+app.use("/v1", v1);
---
+++ v1.mjs
@@ -0,0 +1,82 @@
+import { ObjectId } from "mongodb";
+import products from "./conn.mjs";
+import express from "express";
+import { getFiltros, getSortByDate } from "./help.mjs";
+
+const app = express.Router();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  try {
+    const filtro = getFiltros(req);
+    const sortOption = getSortByDate(req);
+
+    const offset = parseInt(req.query.offset) || 0;
+    const limit = parseInt(req.query.limit) || 0;
+
+    let results = await products
+      .find(filtro)
+      .sort(sortOption)
+      .skip(offset)
+      .limit(limit)
+      .toArray();
+    res.send(results).status(200);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const product = req.body;
+    const result = await products.insertOne(product);
+    res.send(result).status(200);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
+
+app.get("/:id", async (req, res) => {
+  try {
+    const result = await products.findOne({ _id: new ObjectId(req.params.id) });
+    res.send(result).status(200);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
+
+app.delete("/:id", async (req, res) => {
+  try {
+    const result = await products.deleteOne({
+      _id: new ObjectId(req.params.id),
+    });
+    res.send(result).status(200);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
+
+app.delete("/", async (req, res) => {
+  try {
+    let result = await products.deleteMany(req.body);
+    res.send(result).status(200);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
+
+app.put("/:id", async (req, res) => {
+  try {
+    const result = await products.updateOne(
+      { _id: new ObjectId(req.params.id) },
+      { $set: req.body }
+    );
+    res.send(result).status(200);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
+
+export default app;
