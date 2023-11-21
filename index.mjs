4cc636db152da3bb7c46485e9bc084d79f32f253
4ntoniofr <afr012240@uma.es>
2023-11-21 20:32:18 +0100

    cors policy error solved

--- index.mjs
+++ index.mjs
@@ -1,9 +1,11 @@
 import express from "express";
 import v1 from "./v1.mjs";
+import cors from "cors";
 
 const app = express();
 const port = 5001;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
