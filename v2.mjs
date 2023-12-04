c086479fec10a7fe4a5d37e8dd9db1b34d4d2edd
Valentín <valentingr080@uma.es>
2023-12-04 16:54:12 +0100

    filter with lat long radius in products

--- v2.mjs
+++ v2.mjs
@@ -9,6 +9,30 @@ const app = express.Router();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", async (req, res) => {
+  if(req.query.lat && req.query.long && req.query.radius){
+    const clientPetition = await fetch(`http://localhost:5000/v1/?lat=${req.query.lat}&long=${req.query.long}&radius=${req.query.radius}`);
+    const result_cliente = await clientPetition.json();
+    // make a list with the ids of the clients in order to add them to the filter
+    const clientIds = result_cliente.map((client) => client._id);
+    let filtros = { userID: { $in: clientIds } };
+    if (req.query.name) {
+      filtros = {
+        ...filtros,
+        name: { $regex: req.query.name, $options: "i" },
+      };
+    }
+    const result = await products.find(filtros).toArray();
+    res.send(result).status(200);
+  }
+  else{
+    const filtros = getFiltros(req.query);
+    const sort = getSortByDate(req.query);
+    const results = await products.find(filtros).sort(sort).toArray();
+    res.send(results).status(200);
+  }
+});
+
 app.get("/:id1/:id2", async (req, res) => {
     try {
       const results = await products.find({$or: [
