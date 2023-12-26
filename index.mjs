3af65d2521d91da64a8f2dfc2e9a1cbe7250121d
VALEN-PC\arion <valentingr080@gmail.com>
2023-12-26 17:46:09 +0100

    cors changed

--- index.mjs
+++ index.mjs
@@ -9,45 +9,53 @@ import products from "./conn.mjs";
 const app = express();
 const port = 5001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-    console.log(`Now listening on port ${port}`);
+  console.log(`Now listening on port ${port}`);
 });
 
-const clients = process.env.CLIENTS != undefined ? process.env.CLIENTS : "localhost";
+const clients =
+  process.env.CLIENTS != undefined ? process.env.CLIENTS : "localhost";
 
 const verifyToken = async (req, res, next) => {
-    try {
-        if (req.method != "GET") {
-            const response = await axios.get(`http://${clients}:5000/checkToken/${req.headers.authorization}`);
-            const user = response.data.user;
+  try {
+    if (req.method != "GET") {
+      const response = await axios.get(
+        `http://${clients}:5000/checkToken/${req.headers.authorization}`
+      );
+      const user = response.data.user;
 
-            if (req.params.id != null) {
-                const product = await products.findOne({ _id: new ObjectId(req.params.id) });
-                if (product.userID != user._id) {
-                    res.status(402).send("Unauthorized action");
-                    return;
-                }
-                else if (req.params.id1 != undefined && req.params.id2 != undefined) {
-                    if (req.params.id1 != user._id && req.params.id2 != user._id) {
-                        res.status(402).send("Unauthorized action");
-                        return;
-                    }
-                }
-            } else if (req.method == 'POST' && req.body.userID != user._id) {
-                res.status(402).send("Unauthorized action");
-                return;
-            }
+      if (req.params.id != null) {
+        const product = await products.findOne({
+          _id: new ObjectId(req.params.id),
+        });
+        if (product.userID != user._id) {
+          res.status(402).send("Unauthorized action");
+          return;
+        } else if (req.params.id1 != undefined && req.params.id2 != undefined) {
+          if (req.params.id1 != user._id && req.params.id2 != user._id) {
+            res.status(402).send("Unauthorized action");
+            return;
+          }
         }
-
-        next();
-    } catch {
-        res.status(401).send({ error: "Invalid token" });
+      } else if (req.method == "POST" && req.body.userID != user._id) {
+        res.status(402).send("Unauthorized action");
+        return;
+      }
     }
-}
+
+    next();
+  } catch {
+    res.status(401).send({ error: "Invalid token" });
+  }
+};
 
 app.use("/v1", verifyToken, v1);
 app.use("/v2", verifyToken, v2);
